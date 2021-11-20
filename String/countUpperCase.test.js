f14239ff19af8304bf6bfbb17fa22df180256a6f
zeromountain <sonyeongsan@gmail.com>
2021-11-20 16:16:28 +0900

    Solve countUpperCase problem

---
+++ String/countUpperCase.test.js
@@ -0,0 +1,19 @@
+const countUpperCase = function (str) {
+  return str.match(/[A-Z]/g).length;
+};
+
+test(`'KoreaTimeGood'의 문자에 대문자가 몇 개?!`, () => {
+  expect(countUpperCase('KoreaTimeGood')).toBe(3);
+});
+test(`'APplE'의 문자에 대문자가 몇 개?!`, () => {
+  expect(countUpperCase('APplE')).toBe(3);
+});
+test(`'AirPLAnE'의 문자에 대문자가 몇 개?!`, () => {
+  expect(countUpperCase('AirPLAnE')).toBe(5);
+});
+test(`'AbCdEfGhIjKlMnOpQrStUvWxYz'의 문자에 대문자가 몇 개?!`, () => {
+  expect(countUpperCase('AbCdEfGhIjKlMnOpQrStUvWxYz')).toBe(13);
+});
+test(`'How Are You?'의 문자에 대문자가 몇 개?!`, () => {
+  expect(countUpperCase('How Are You?')).toBe(3);
+});

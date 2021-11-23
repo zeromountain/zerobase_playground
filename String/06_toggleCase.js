d410267b4a22a7c1eb7262fb2c5b57320c50d833
zeromountain <sonyeongsan@gmail.com>
2021-11-23 09:52:51 +0900

    toggleCase: match → replace

--- String/06_toggleCase.js
+++ String/06_toggleCase.js
@@ -1,11 +1,15 @@
+// const toggleCase = str =>
+//   str
+//     .match(/[a-zA-z]/g)
+//     .map(word => {
+//       return word === word.toUpperCase()
+//         ? word.toLowerCase()
+//         : word.toUpperCase();
+//     })
+//     .join('');
 const toggleCase = str =>
-  str
-    .match(/[a-zA-z]/g)
-    .map(word => {
-      return word === word.toUpperCase()
-        ? word.toLowerCase()
-        : word.toUpperCase();
-    })
-    .join('');
+  str.replace(/[a-zA-Z]/g, word =>
+    word === word.toUpperCase() ? word.toLowerCase() : word.toUpperCase(),
+  );
 
 module.exports = toggleCase;
